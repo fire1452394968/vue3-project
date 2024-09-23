13d22e8040c26398c0ac6c4507d932600789c38e
fireliangjiang <fireliangjiang@gmail.com>
2024-09-23 22:36:17 +0800

    ✨ feat( 新依赖):

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,19 @@
 /*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Author: fireliangjiang fireliangjiang@gmail.com
  * @Date: 2024-09-23 16:51:38
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-09-23 17:12:26
+ * @LastEditors: fireliangjiang fireliangjiang@gmail.com
+ * @LastEditTime: 2024-09-23 18:53:33
  * @FilePath: \vue3-project\src\router\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
+    name: 'homeView',
     component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: fireliangjiang fireliangjiang@gmail.com
+ * @Date: 2024-09-23 18:53:21
+ * @LastEditors: fireliangjiang fireliangjiang@gmail.com
+ * @LastEditTime: 2024-09-23 18:55:19
+ * @FilePath: \vue3-project\src\views\HomeView.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    <v-chip-group>
+      <v-chip>Chip 1</v-chip>
+
+      <v-chip>Chip 2</v-chip>
+
+      <v-chip>Chip 3</v-chip>
+    </v-chip-group>
+  </div>
+</template>
+
+<script setup lang='ts'>
+</script>
+
+<style scoped lang='less'>
+</style>
